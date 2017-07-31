8954f5fb47374446684f36d2345545473d51531d
Michael Dommett <michaeldommett1@gmail.com>
2017-07-31 14:36:56 +0100

    now much faster

--- g09_scan_collect.py
+++ g09_scan_collect.py
@@ -1,83 +1,50 @@
 #!/usr/bin/env python
 
-# Script to extract the energies from an opt=modredundant Gaussian 09 scan
-# job
-
+from sys import argv,exit
+from periodic import element
 import matplotlib.pyplot as plt
-import numpy as np
-from sys import argv
+from datetime import datetime
+startTime = datetime.now()
 
-# Input and output files
-infile = open(argv[1],"r").read().splitlines()
-outfile = open("energies_"+argv[1],"w")
+finp = open(argv[1],"r").read().splitlines()
+fout = open(argv[1]+"test","w")
 
-scf = []
-for line in infile:
-    split_line = line.split()
-    if len(split_line) == 9:
-        if split_line[0] == 'SCF':
-            scf.append(float(split_line[4]))
-    if len(split_line) == 5:
-        if split_line[0] == 'Total':
-            scf.append(float(split_line[4]))
-    if len(split_line) == 2:
-        if split_line[0] == 'Optimization':
-            scf.append(str(split_line[0]))
+lines = filter(None, (line.rstrip() for line in finp))
+hold_scf=[]
+hold_s1=[]
 
+for j in lines:
 
-count = 0
-s0_au = []
-s1_au = []
+    if  " SCF Done:" in j:
+        scf=float(j.split()[4])
 
-for i in scf:
-    if isinstance(i, basestring):
-        s0_au.append(scf[count-2])
-        s1_au.append(scf[count-1])
-    count +=1
+    if " Total Energy" in j:
+        s1=float(j.split()[4])
 
-min_au = float(min(s0_au))
+    if j==" Optimization completed.":
+        hold_scf.append(scf)
+        hold_s1.append(s1)
+        del scf,s1
 
-s0_ev = []
-s1_ev = []
+scf_final=[(i-hold_scf[0])*27.2114 for i in hold_scf]
+s1_final=[(i-hold_scf[0])*27.2114 for i in hold_s1]
 
-for i in s0_au:
-    ev = (float(i)-min_au)*27.2114
-    s0_ev.append(ev)
-for i in s1_au:
-    ev = (float(i)-min_au)*27.2114
-    s1_ev.append(ev)
-
-#outfile.write(" s0       s1\n")
-
-count=0
-for i in s0_ev:
-    outfile.write("{0:2.3f}    {1:2.3f}  \n".format(i,s1_ev[count]))
-    count+=1
-
-
-
-x = range(1,len(s0_au)+1)
-
+if len(scf_final)==len(s1_final):
+        for i in range(len(scf_final)):
+            fout.write("{0:>2.3f}  {1:>2.3f}\n".format(scf_final[i],s1_final[i]))
+else:
+    sys.exit("\n\nCheck the G09 output file, there is a problem\n\n")
+print datetime.now() - startTime
 plotgraph = raw_input("\nDo you want to plot a graph? Yes/No : ")
-
-if plotgraph=="Yes":
-
+x=range(len(s1_final))
+if plotgraph in ("yes","YES","Yes","y","Y"):
     fig,ax = plt.subplots()
-    ax.plot(x,s0_ev,'o-b',linewidth=2)
-    ax.plot(x,s1_ev,'o-r',linewidth=2)
-    #ax.spines['right'].set_visible(False)
-    #ax.spines['top'].set_visible(False)
-    #ax.spines['left'].set_linewidth(1.5)
-    #ax.spines['bottom'].set_linewidth(1.5)
-    #ax.xaxis.set_ticks_position('bottom')
-    #ax.yaxis.set_ticks_position('left')
-    #ax.xaxis.set_tick_params(labelsize=12, width=1.5)
-    #ax.yaxis.set_tick_params(labelsize=12, width=1.5)
+    ax.plot(x,scf_final,'o-b',linewidth=2)
+    ax.plot(x,s1_final,'o-r',linewidth=2)
     plt.xlabel('X', fontsize=12)
     plt.ylabel('Energy (eV)', fontsize=12)
     plt.show()
-
-elif plotgraph=="No":
+elif plotgraph in ("no","NO","No","n","N"):
     print "\nOkay, I will just print the energies\n"
 else:
     print "\nI did not understand your answer. Please enter 'Yes' or 'No'\n"
