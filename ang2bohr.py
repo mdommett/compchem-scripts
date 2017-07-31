93bd41d001fb3f24a3505851ebdb63aa11aae423
Michael Dommett <michaeldommett1@gmail.com>
2017-07-31 11:26:57 +0100

    uses standard xyz files

--- ang2bohr.py
+++ ang2bohr.py
@@ -6,7 +6,10 @@ from sys import argv
 ang2bohr=1.88973
 infile=open(argv[1],"r").read().splitlines()
 outfile=open(argv[1]+"-bohr","w")
+
+for i in infile[:3]:
+    outfile.write("{}\n".format(i))
 for i in infile[2:]:
     ix=i.split()
-    outfile.write("{0:<2} {1:>13.9f}   {2:>13.9f}   {3:>13.9f}   {4:>13.9}\n".format(ix[0],float(ix[1])*ang2bohr,float(ix[2])*ang2bohr,float(ix[3])*ang2bohr,ix[4]))
+    outfile.write("{0:<2} {1:>13.9f}   {2:>13.9f}   {3:>13.9f}\n".format(ix[0],float(ix[1])*ang2bohr,float(ix[2])*ang2bohr,float(ix[3])*ang2bohr))
 print datetime.now() - startTime
