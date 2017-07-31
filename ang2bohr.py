0b26e5e0f8b4e68026117af48a3a9ed1121560a5
Michael Dommett <michaeldommett1@gmail.com>
2017-07-31 11:22:16 +0100

    reads xyz files as input

--- ang2bohr.py
+++ ang2bohr.py
@@ -6,7 +6,7 @@ from sys import argv
 ang2bohr=1.88973
 infile=open(argv[1],"r").read().splitlines()
 outfile=open(argv[1]+"-bohr","w")
-for i in infile:
+for i in infile[2:]:
     ix=i.split()
-    outfile.write("{0:>13.9f}   {1:>13.9f}   {2:>13.9f}   {3:>13.9}\n".format(float(ix[0])*ang2bohr,float(ix[1])*ang2bohr,float(ix[2])*ang2bohr,ix[3]))
+    outfile.write("{0:<2} {1:>13.9f}   {2:>13.9f}   {3:>13.9f}   {4:>13.9}\n".format(ix[0],float(ix[1])*ang2bohr,float(ix[2])*ang2bohr,float(ix[3])*ang2bohr,ix[4]))
 print datetime.now() - startTime
