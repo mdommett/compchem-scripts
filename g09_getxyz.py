5af933f4f26082f8f6b3aab1491aa12f6ff18517
Michael Dommett <michaeldommett1@gmail.com>
2017-11-07 14:09:10 +0000

    removes .out suffix

--- g09_getxyz.py
+++ g09_getxyz.py
@@ -5,12 +5,12 @@ from periodic import element
 
 
 finp = open(argv[1],"r").read().splitlines()
-fout = open(argv[1]+".xyz","w")
+fout = open(argv[1][:-4]+".xyz","w")
 #natoms = int(raw_input("How many atoms are in high layer? "))
 lines = filter(None, (line.rstrip() for line in finp))
 for i,j in enumerate(lines):
     if j.split()[0]=="NAtoms=":
-	natoms=int(j.split()[3]) 
+	natoms=int(j.split()[3])
     if j == " Number     Number       Type             X           Y           Z":
         count = int(i)
         fout.write("{}\n\n".format(natoms))
