26b4cde940275c0505452b3c0988e09f427b507d
Michael Dommett <michaeldommett1@gmail.com>
2017-11-16 16:19:25 +0000

    optimises

--- g09_UFF_input.py
+++ g09_UFF_input.py
@@ -37,7 +37,7 @@ comfile = open(jobname+".com","w")
 chk = "%chk={0}.chk".format(jobname)
 nproc = "%nproc=4"
 mem = "%mem=28GB"
-input_line = "#p ONIOM(wb97xd/6-31G*:UFF=QEq)=(EmbedCharge) nosymm"
+input_line = "#p ONIOM(wb97xd/6-31G*:UFF=QEq)=(EmbedCharge) opt nosymm"
 comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n".format(chk,nproc,mem,input_line))
 for atom in final_cluster[:natoms]:
     atomStr = "{:>6} 0 {:10.6f} {:10.6f} {:10.6f} H \n".format(atom.elem, atom.x, atom.y, atom.z)
@@ -48,6 +48,3 @@ for atom in final_cluster[natoms:]:
 comfile.write("\n--link1--\n")
 input_line="#p ONIOM(wb97xd/6-311++G** td=(nstates=3):UFF=QEq)=(EmbedCharge) geom=check guess=read nosymm"
 comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n\n".format(chk,nproc,mem,input_line))
-
-
-ef.write_xyz("test.xyz",cluster)
