6ea2e2446c777d80145fc53ec2da2b7dc0fae564
Michael Dommett <michaeldommett1@gmail.com>
2017-11-20 11:49:22 +0000

    force calc

--- g09_UFF_input.py
+++ g09_UFF_input.py
@@ -50,14 +50,14 @@ print c_m_centroid
 print ha.find_centroid(final_cluster)
 ef.write_xyz(args.input[:-4]+"-cluster.xyz",final_cluster)
 ######################################################################
-"""
+
 jobname=args.input[:-4]+"_UFF"
 comfile = open(jobname+".com","w")
 
 chk = "%chk={0}.chk".format(jobname)
 nproc = "%nproc=4"
 mem = "%mem=28GB"
-input_line = "#p ONIOM(wb97xd/6-31G*:UFF=QEq)=(EmbedCharge) opt nosymm"
+input_line = "#p ONIOM(wb97xd/6-31G*:UFF=QEq)=(EmbedCharge) force nosymm"
 comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n".format(chk,nproc,mem,input_line))
 for atom in final_cluster[:natoms]:
     atomStr = "{:>6} 0 {:10.6f} {:10.6f} {:10.6f} H \n".format(atom.elem, atom.x, atom.y, atom.z)
@@ -65,7 +65,6 @@ for atom in final_cluster[:natoms]:
 for atom in final_cluster[natoms:]:
     atomStr = "{:>6} -1 {:10.6f} {:10.6f} {:10.6f} M \n".format(atom.elem, atom.x, atom.y, atom.z)
     comfile.write(atomStr)
-comfile.write("\n--link1--\n")
-input_line="#p ONIOM(wb97xd/6-311++G** td=(nstates=3):UFF=QEq)=(EmbedCharge) geom=check guess=read nosymm"
-comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n\n".format(chk,nproc,mem,input_line))
-"""######################################################################
+#comfile.write("\n--link1--\n")
+#input_line="#p ONIOM(wb97xd/6-311++G** td=(nstates=3):UFF=QEq)=(EmbedCharge) geom=check guess=read nosymm"
+#comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n\n".format(chk,nproc,mem,input_line))
