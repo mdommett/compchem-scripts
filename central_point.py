38069cc9aed46e47d7bcd7e5e12c895854d24fc3
Michael Dommett <michaeldommett1@gmail.com>
2017-12-06 17:44:36 +0000

    reads xyz

--- central_point.py
+++ central_point.py
@@ -3,13 +3,13 @@
 import numpy as np
 from sys import argv
 
-xyz = open(argv[1],'r')[2:]
+xyz = open(argv[1],'r').read().splitlines()
 
 xc =[]
 yc =[]
 zc =[]
 
-for line in xyz:
+for line in xyz[2:]:
     ln = line.split()
     xc.append(float(ln[1]))
     yc.append(float(ln[2]))
