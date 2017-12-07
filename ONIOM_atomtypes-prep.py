827409346d743433142a55b729e9b2ceccda198c
Michael Dommett <michaeldommett1@gmail.com>
2017-12-07 09:52:49 +0000

    preps atom types for amber

---
+++ ONIOM_atomtypes-prep.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from sys import argv,exit
+atoms=open("atoms","read").read().splitlines()
+types=open("atomtypes","read").read().splitlines()
+
+with open("atomtypes-complete","w") as atomtypes_tmp:
+    for i in range(len(atoms)):
+        atomtypes_tmp.write("{}-{}\n".format(atoms[i],str(types[i].split()[0])))
+    atomtypes_tmp.close()
