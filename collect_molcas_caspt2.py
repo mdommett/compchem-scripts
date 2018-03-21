e976273709d24a0b49de4c3c1723239a6f95e9f9
Michael Dommett <michaeldommett1@gmail.com>
2018-03-21 21:46:29 +0000

    correct string being taken

--- collect_molcas_caspt2.py
+++ collect_molcas_caspt2.py
@@ -6,12 +6,12 @@ with open(sys.argv[1],"r") as infile:
     energies=[]
     for line in infile:
         if "MS-CASPT2 Root" in line:
-            energies.append(float(line.split()[5]))
+            energies.append(float(line.split()[6]))
 if not energies:
     sys.exit("Couldn't find any MS-CASPT2 roots!")
 else:
     energies_ev=[(i-energies[0])*27.2114 for i in energies]
 
-print("Energies of MS-CASPT2 states:\n")
+print("Energies of MS-CASPT2 states:")
 for i,energy in enumerate(energies_ev):
-    print("{0}: {1:.2f} eV".format(i,energy))
+    print("S{0}: {1:.2f} eV".format(i,energy))
