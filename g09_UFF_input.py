a87d954f7438fb182819b518aa1e65460754d9d7
Michael Dommett <michaeldommett1@gmail.com>
2018-03-26 17:04:05 +0100

    added name=main run prereq

--- g09_UFF_input.py
+++ g09_UFF_input.py
@@ -28,52 +28,52 @@ def make_clust(atoms, clust_rad, max_bl, refx, refy, refz,natoms):
                     clust_atoms.append(atom2Add)
     return clust_atoms
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Input .xyz file",type=str)
+    parser.add_argument("-b", "--bond", help="Maximum length (in unites of input file) that qualifies as a bond",
+                            default=1.6, type=float)
+    parser.add_argument("-r", "--radius", help="Radius of cluster",type=float,default=10)
+    user_input = sys.argv[1:]
+    args = parser.parse_args(user_input)
+    cluster=rf.read_xyz(args.input)[-1]
+    cluster_centroid=ha.find_centroid(cluster)
+    distances=[ds.vector_distance(cluster_centroid[0],cluster_centroid[1],cluster_centroid[2],atom.x,atom.y,atom.z) for atom in cluster]
+    central_atom=cluster[distances.index(min(distances))]
+    central_molecule=ha.select(args.bond,cluster,distances.index(min(distances)))
+    natoms=len(central_molecule)
+    for i in central_molecule:
+        cluster.remove(i)
+        cluster.insert(0,i)
+    c_m_centroid=ha.find_centroid(central_molecule)
+    cluster_start=time.time()
+    final_cluster=make_clust(cluster,args.radius,args.bond,c_m_centroid[0],c_m_centroid[1],c_m_centroid[2],natoms)
+    cluster_end=time.time()
+    print("Time to make cluster: {}s".format(cluster_end-cluster_start))
+    ef.write_xyz(args.input[:-4]+"-cluster.xyz",final_cluster)
+    final_cluster_centroid=ha.find_centroid(final_cluster)
+    print("Cluster centroid: {} {} {}".format(final_cluster_centroid[0],final_cluster_centroid[1],final_cluster_centroid[2]))
+    print("Central molecule centroid: {} {} {}".format(c_m_centroid[0],c_m_centroid[1],c_m_centroid[2]))
+    ######################################################################
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input", help="Input .xyz file",type=str)
-parser.add_argument("-b", "--bond", help="Maximum length (in unites of input file) that qualifies as a bond",
-                        default=1.6, type=float)
-parser.add_argument("-r", "--radius", help="Radius of cluster",type=float,default=10)
-user_input = sys.argv[1:]
-args = parser.parse_args(user_input)
-cluster=rf.read_xyz(args.input)[-1]
-cluster_centroid=ha.find_centroid(cluster)
-distances=[ds.vector_distance(cluster_centroid[0],cluster_centroid[1],cluster_centroid[2],atom.x,atom.y,atom.z) for atom in cluster]
-central_atom=cluster[distances.index(min(distances))]
-central_molecule=ha.select(args.bond,cluster,distances.index(min(distances)))
-natoms=len(central_molecule)
-for i in central_molecule:
-    cluster.remove(i)
-    cluster.insert(0,i)
-c_m_centroid=ha.find_centroid(central_molecule)
-cluster_start=time.time()
-final_cluster=make_clust(cluster,args.radius,args.bond,c_m_centroid[0],c_m_centroid[1],c_m_centroid[2],natoms)
-cluster_end=time.time()
-print("Time to make cluster: {}s".format(cluster_end-cluster_start))
-ef.write_xyz(args.input[:-4]+"-cluster.xyz",final_cluster)
-final_cluster_centroid=ha.find_centroid(final_cluster)
-print("Cluster centroid: {} {} {}".format(final_cluster_centroid[0],final_cluster_centroid[1],final_cluster_centroid[2]))
-print("Central molecule centroid: {} {} {}".format(c_m_centroid[0],c_m_centroid[1],c_m_centroid[2]))
-######################################################################
+    jobname=args.input[:-4]+"_UFF"
+    comfile = open(jobname+".com","w")
 
-jobname=args.input[:-4]+"_UFF"
-comfile = open(jobname+".com","w")
+    chk = "%chk={0}.chk".format(jobname)
+    nproc = "%nproc=4"
+    mem = "%mem=28GB"
+    input_line = "#p ONIOM(wb97xd/6-31G*:UFF=QEq)=(EmbedCharge) opt nosymm"
+    comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n".format(chk,nproc,mem,input_line))
+    for atom in final_cluster[:natoms]:
+        atomStr = "{:>6} 0 {:10.6f} {:10.6f} {:10.6f} H \n".format(atom.elem, atom.x, atom.y, atom.z)
+        comfile.write(atomStr)
+    for atom in final_cluster[natoms:]:
+        atomStr = "{:>6} -1 {:10.6f} {:10.6f} {:10.6f} M \n".format(atom.elem, atom.x, atom.y, atom.z)
+        comfile.write(atomStr)
+    #comfile.write("\n")
 
-chk = "%chk={0}.chk".format(jobname)
-nproc = "%nproc=4"
-mem = "%mem=28GB"
-input_line = "#p ONIOM(wb97xd/6-31G*:UFF=QEq)=(EmbedCharge) opt nosymm"
-comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n".format(chk,nproc,mem,input_line))
-for atom in final_cluster[:natoms]:
-    atomStr = "{:>6} 0 {:10.6f} {:10.6f} {:10.6f} H \n".format(atom.elem, atom.x, atom.y, atom.z)
-    comfile.write(atomStr)
-for atom in final_cluster[natoms:]:
-    atomStr = "{:>6} -1 {:10.6f} {:10.6f} {:10.6f} M \n".format(atom.elem, atom.x, atom.y, atom.z)
-    comfile.write(atomStr)
-#comfile.write("\n")
-
-comfile.write("\n--link1--\n")
-input_line="#p ONIOM(wb97xd/6-311++G** td=(nstates=3):UFF=QEq)=(EmbedCharge) geom=check guess=read nosymm"
-comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n\n".format(chk,nproc,mem,input_line))
-end = time.time()
-print("\nTotal time: {}s".format(end - start))
+    comfile.write("\n--link1--\n")
+    input_line="#p ONIOM(wb97xd/6-311++G** td=(nstates=3):UFF=QEq)=(EmbedCharge) geom=check guess=read nosymm"
+    comfile.write("{0}\n{1}\n{2}\n{3}\n\n Title \n\n0 1\n\n".format(chk,nproc,mem,input_line))
+    end = time.time()
+    print("\nTotal time: {}s".format(end - start))
