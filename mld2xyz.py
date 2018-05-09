e3429cecdfdc2523a8b4dc3399e1043dc72ffde5
Michael Dommett <michaeldommett1@gmail.com>
2018-05-09 14:22:52 +0100

    made start conidition more general by rmeoving xyz string from search string

--- mld2xyz.py
+++ mld2xyz.py
@@ -7,7 +7,7 @@ import sys
 infile=open(sys.argv[1],"r").read().splitlines()
 outfile=open(sys.argv[2],"w")
 for i,line in enumerate(infile):
-    if "[GEOMETRIES] (XYZ)" in line:
+    if "[GEOMETRIES]" in line:
         start=i+1
     if "[FORCES]" in line:
         stop=i
