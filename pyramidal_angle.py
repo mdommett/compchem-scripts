b2b9cdc5e68c1f734e30faff7ba6209410ea21f7
Michael Dommett <michaeldommett1@gmail.com>
2018-05-21 23:19:39 +0100

    pyramidal angle measure

---
+++ pyramidal_angle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import read_file as rf
+from sys import argv
+import numpy as np
+import edit_file as ef
+
+"""
+    A
+    |
+ C--B--D
+"""
+
+def coordinate_matrix(atoms):
+    coords=np.zeros((len(atoms),3))
+    for i in range(len(atoms)):
+        coords[i,0]=atoms[i].x
+        coords[i,1]=atoms[i].y
+        coords[i,2]=atoms[i].z
+    return coords
+
+def magnitude(vector):
+    return np.linalg.norm(vector)
+
+if __name__=='__main__':
+    geom_file=rf.read_pos(argv[1])
+    geom=coordinate_matrix(geom_file)
+    A=int(argv[2])-1
+    B=int(argv[3])-1
+    C=int(argv[4])-1
+    D=int(argv[5])-1
+
+    BA=geom[A]-geom[B]
+    BC=geom[C]-geom[B]
+    BD=geom[D]-geom[B]
+
+    angle=np.arccos(np.dot(np.cross(BC,BD),BA)/np.dot(magnitude(np.cross(BC,BD)),magnitude(BA)))
+    pyr_angle_radians=np.pi/2 - angle
+    pyr_angle=np.degrees(pyr_angle_radians)
+    print(pyr_angle)
